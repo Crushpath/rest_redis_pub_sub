9ecaf044370bc8ebc94685b77b03572c14374398
Jose Corcuera <jose@crushpath.com>
2014-05-20 10:29:59 -0500

    Specs for created event. #7

--- spec/rest_redis_pub_sub/client_spec.rb
+++ spec/rest_redis_pub_sub/client_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe RestRedisPubSub::Client do
+  before do
+    @redis_instance = double('redis_instance')
+    RestRedisPubSub.redis_instance = @redis_instance
+    RestRedisPubSub.publisher = 'my-app'
+    @expected_data = {
+      publisher: RestRedisPubSub.publisher,
+      resource: 'resource',
+      identifier: 'resource-id',
+      data: {name: 'resource-name'}
+    }
+  end
+
+  describe ".created" do
+    it "publish a created event for the given resource" do
+      json_message = @expected_data.merge(event: :created).to_json
+      expect(@redis_instance).to receive(:publish).with('my-app-resource', json_message)
+
+      client = RestRedisPubSub::Client.new('my-app-resource')
+      client.created('resource', 'resource-id', {name: 'resource-name'})
+    end
+  end
 
 end
