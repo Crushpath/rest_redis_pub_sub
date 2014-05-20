f5f9fb943367a194bb9daa818f5966b54db7d975
Jose Corcuera <jose@crushpath.com>
2014-05-20 10:37:04 -0500

    Publish proper events. #7

--- spec/rest_redis_pub_sub/client_spec.rb
+++ spec/rest_redis_pub_sub/client_spec.rb
@@ -7,19 +7,43 @@ describe RestRedisPubSub::Client do
     RestRedisPubSub.publisher = 'my-app'
     @expected_data = {
       publisher: RestRedisPubSub.publisher,
+      event: nil,
       resource: 'resource',
-      identifier: 'resource-id',
+      id: 'resource-id',
       data: {name: 'resource-name'}
     }
   end
 
   describe ".created" do
     it "publish a created event for the given resource" do
-      json_message = @expected_data.merge(event: :created).to_json
+      @expected_data[:event] = :created
+      json_message = @expected_data.to_json
       expect(@redis_instance).to receive(:publish).with('my-app-resource', json_message)
 
       client = RestRedisPubSub::Client.new('my-app-resource')
       client.created('resource', 'resource-id', {name: 'resource-name'})
+    end
+  end
+
+  describe ".updated" do
+    it "publish a updated event for the given resource" do
+      @expected_data[:event] = :updated
+      json_message = @expected_data.to_json
+      expect(@redis_instance).to receive(:publish).with('my-app-resource', json_message)
+
+      client = RestRedisPubSub::Client.new('my-app-resource')
+      client.updated('resource', 'resource-id', {name: 'resource-name'})
+    end
+  end
+
+  describe ".deleted" do
+    it "publish a deleted event for the given resource" do
+      @expected_data[:event] = :deleted
+      json_message = @expected_data.to_json
+      expect(@redis_instance).to receive(:publish).with('my-app-resource', json_message)
+
+      client = RestRedisPubSub::Client.new('my-app-resource')
+      client.deleted('resource', 'resource-id', {name: 'resource-name'})
     end
   end
 
