ff0158a94af10c62774117793515d427648a7197
Jose Corcuera <jose@crushpath.com>
2014-05-22 18:27:12 -0500

    Include publisher in class forwarded. #20

--- spec/rest_redis_pub_sub/event_handler_spec.rb
+++ spec/rest_redis_pub_sub/event_handler_spec.rb
@@ -26,23 +26,35 @@ describe RestRedisPubSub::EventHandler do
 
   describe "#class_to_forward" do
     it "should retrieve the proper class if exists" do
-      ProductCreated = Class.new
-      handler = RestRedisPubSub::EventHandler.new('event' => 'created', 'resource' => 'product')
-      expect(handler.class_to_forward).to eq(ProductCreated)
+      MyAppProductCreated = Class.new
+      handler = RestRedisPubSub::EventHandler.new(
+        'publisher' => 'my_app',
+        'event' => 'created',
+        'resource' => 'product'
+      )
+      expect(handler.class_to_forward).to eq(MyAppProductCreated)
     end
 
     it "should return nil if class doesnt exists" do
-      handler = RestRedisPubSub::EventHandler.new('event' => 'updated', 'resource' => 'product')
+      handler = RestRedisPubSub::EventHandler.new(
+        'publisher' => 'my_app',
+        'event' => 'updated',
+        'resource' => 'product'
+      )
       expect(handler.class_to_forward).to be_nil
     end
 
     it "should consider listener_namespace if set" do
       MyListeners = Module.new
-      MyListeners::ProductDeleted = Class.new
+      MyListeners::MyAppProductDeleted = Class.new
 
       RestRedisPubSub.listeners_namespace = MyListeners
-      handler = RestRedisPubSub::EventHandler.new('event' => 'deleted', 'resource' => 'product')
-      expect(handler.class_to_forward).to eq(MyListeners::ProductDeleted)
+      handler = RestRedisPubSub::EventHandler.new(
+        'publisher' => 'my_app',
+        'event' => 'deleted',
+        'resource' => 'product'
+      )
+      expect(handler.class_to_forward).to eq(MyListeners::MyAppProductDeleted)
     end
   end
 
