4add4ca60e210b7e454fa931135eac9e13ecdd0b
Jose Corcuera <jose@crushpath.com>
2014-05-26 10:52:50 -0500

    Use generator instead of publisher. #24

--- lib/rest_redis_pub_sub/configuration.rb
+++ lib/rest_redis_pub_sub/configuration.rb
@@ -4,13 +4,13 @@ module RestRedisPubSub
       block.call(self)
     end
 
-    attr_accessor :subscribe_to, :publish_to, :publisher,
+    attr_accessor :subscribe_to, :publish_to, :generator,
                   :listeners_namespace, :redis_instance
 
     def reset!
       @subscribe_to = nil
       @publish_to = nil
-      @publisher = nil
+      @generator = nil
       @listeners_namespace = nil
       @redis_instance = nil
     end
